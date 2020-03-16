a49d225910e1a89490b6c9551b6f4e0791020c66
Marco <marco.teixeiradasilva@hotmail.fr>
2020-03-16 14:39:27 +0100

    initialisation

--- index.php
+++ index.php
@@ -7,3 +7,41 @@ include_once 'codebarre.class.php';
 // $codeBarre = new CodeBarre("12345678");
 
 var_dump(str_split("12345678"));
+
+
+$elementA = [];
+$elementA[0] = "0001101";
+$elementA[1] = "0011001";
+$elementA[2] = "0010011";
+$elementA[3] = "0111101";
+$elementA[4] = "0100011";
+$elementA[5] = "0110001";
+$elementA[6] = "0101111";
+$elementA[7] = "0111011";
+$elementA[8] = "0110111";
+$elementA[9] = "0001011";
+
+$elementB = [];
+$elementB[0] = "0100111";
+$elementB[1] = "0110011";
+$elementB[2] = "0110011";
+$elementB[3] = "0011011";
+$elementB[4] = "0100001";
+$elementB[5] = "0011101";
+$elementB[6] = "";
+$elementB[7] = "";
+$elementB[8] = "";
+$elementB[9] = "";
+
+$elementC = [];
+$elementC[0] = '';
+$elementC[1] = '';
+$elementC[2] = '';
+$elementC[3] = '';
+$elementC[4] = '';
+$elementC[5] = '';
+$elementC[6] = '';
+$elementC[7] = '';
+$elementC[8] = '';
+$elementC[9] = '';
+}
